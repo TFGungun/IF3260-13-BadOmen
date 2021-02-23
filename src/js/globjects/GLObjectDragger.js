31819a424d5cab80727e0c2a7ac625ccbc894295
Gimhafnu <13518127@std.stei.itb.ac.id>
2021-02-23 23:22:55 +0700

    Vertices are now draggable
    Still no handling for... scaling, rotation and translation tho... also... there seems to be a weird bug with dept...

---
+++ src/js/globjects/GLObjectDragger.js
@@ -0,0 +1,78 @@
+class GLObjectDragger extends GLObject {
+  constructor(id, shader, gl) {
+    super(id, shader, gl);
+
+    this.setVertexArray([
+      -7.5,
+      -7.5, // left bot
+
+      7.5,
+      -7.5, // right bot
+
+      -7.5,
+      7.5, // left top
+
+      -7.5,
+      7.5, // left top
+
+      7.5,
+      -7.5, // right bot
+
+      7.5,
+      7.5, // right top
+    ]);
+
+    this.SetColorByArray([0.7, 0.7, 1, 1.0]);
+    this.setRotation(0);
+    this.setScale(1, 1);
+
+    this.vertId = -1;
+  }
+
+  setVertexId(id) {
+    this.vertId = id;
+  }
+
+  draw() {
+    //
+    const gl = this.gl;
+    gl.useProgram(this.shader);
+    var vertexPos = gl.getAttribLocation(this.shader, "a_pos");
+    var uniformCol = gl.getUniformLocation(this.shader, "u_fragColor");
+    var uniformPos = gl.getUniformLocation(this.shader, "u_proj_mat");
+    gl.vertexAttribPointer(vertexPos, 2, gl.FLOAT, false, 0, 0);
+    gl.uniformMatrix3fv(uniformPos, false, this.projectionMat);
+    gl.uniform4fv(uniformCol, this.color);
+    gl.enableVertexAttribArray(vertexPos);
+    gl.drawArrays(gl.TRIANGLES, 0, 6);
+  }
+
+  drawSelect(selectProgram) {
+    // Gonna delete this later
+    // selectProgram is a WebGL shader program... with selection shaders
+    const gl = this.gl;
+    const id = this.id;
+    gl.useProgram(selectProgram);
+    var vertexPos = gl.getAttribLocation(selectProgram, "a_pos");
+    var uniformCol = gl.getUniformLocation(selectProgram, "u_fragColor");
+    var uniformPos = gl.getUniformLocation(selectProgram, "u_proj_mat");
+    gl.uniformMatrix3fv(uniformPos, false, this.projectionMat);
+    gl.vertexAttribPointer(
+      vertexPos,
+      2, // it's 2 dimensional
+      gl.FLOAT,
+      false,
+      0,
+      0
+    );
+    gl.enableVertexAttribArray(vertexPos);
+    const uniformId = [
+      ((id >> 0) & 0xff) / 0xff,
+      ((id >> 8) & 0xff) / 0xff,
+      ((id >> 16) & 0xff) / 0xff,
+      ((id >> 24) & 0xff) / 0xff,
+    ];
+    gl.uniform4fv(uniformCol, uniformId);
+    gl.drawArrays(gl.TRIANGLES, 0, 6);
+  }
+}
--- src/js/main.js
+++ src/js/main.js
@@ -4,8 +4,10 @@ canvas.height = 600;
 var gl = canvas.getContext("webgl2");
 
 var btnUpdateLineLength = document.getElementById("line-length-btn");
-var btnUpdateSquareSideLength = document.getElementById("square-side-length-btn");
-var btnUpdateColor = document.getElementById("color-update"); 
+var btnUpdateSquareSideLength = document.getElementById(
+  "square-side-length-btn"
+);
+var btnUpdateColor = document.getElementById("color-update");
 // TODO : UBAH ROTATION DAN SCALING KE LOCAL
 // TODO : JANGAN CUMAN SELECT DOANG TAPI BISA TARIK VERTEX
 
@@ -18,9 +20,19 @@ let appState = {
   oldPick: -1,
   selId: -1,
   selectedObject: null,
+  draggedHandle: null,
+  dragStart: {
+    x: -1,
+    y: -1,
+  },
+  dragEnd: {
+    x: -1,
+    y: -1,
+  },
 };
 
 let GLobjectList = [];
+let draggers = [];
 
 function updateLineLength() {
   var newLength;
@@ -31,30 +43,25 @@ function updateLineLength() {
     var pivotY = appState.selectedObject.va[1];
     var xDiff = appState.selectedObject.va[2] - pivotX;
     var yDiff = appState.selectedObject.va[3] - pivotY;
-    var rad = Math.atan(yDiff/xDiff);
+    var rad = Math.atan(yDiff / xDiff);
     var sin = Math.sin(rad);
     var cos = Math.cos(rad);
     console.log("xDiff :" + xDiff);
     console.log("yDiff :" + yDiff);
     console.log("RAD :" + rad);
     console.log("SIN :" + sin);
-    console.log("COS :" +cos);
+    console.log("COS :" + cos);
     console.log("New x :" + parseInt(newLength));
 
     // Set New Length
     appState.selectedObject.va[2] = pivotX + parseInt(newLength) * cos;
     appState.selectedObject.va[3] = pivotY + parseInt(newLength) * sin;
-
-  }
-  else if(newLength == "") {
+  } else if (newLength == "") {
     alert("Input new line length");
-  }
-  else {
+  } else {
     alert("Object selected is not a line");
   }
-
 }
-
 
 function updateRectSideLength() {
   // console.log(appState.selectedObject.typeof);
@@ -67,7 +74,7 @@ function updateRectSideLength() {
     var newLength;
     if (document.getElementById("square-side-length").value != "") {
       newLength = document.getElementById("square-side-length").value;
-      
+
       // Move the Vertices based on size changes
 
       // Bottom-Right Points
@@ -77,8 +84,7 @@ function updateRectSideLength() {
       appState.selectedObject.va[8] = parseInt(pivotX) + parseInt(newLength);
       appState.selectedObject.va[9] = parseInt(pivotY);
 
-      
-     // Top-Left Points
+      // Top-Left Points
       appState.selectedObject.va[4] = parseInt(pivotX);
       appState.selectedObject.va[5] = parseInt(pivotY) + parseInt(newLength);
 
@@ -88,23 +94,105 @@ function updateRectSideLength() {
       // Top-Right Points
       appState.selectedObject.va[10] = parseInt(pivotX) + parseInt(newLength);
       appState.selectedObject.va[11] = parseInt(pivotY) + parseInt(newLength);
-      
-    }
-    else {
+    } else {
       alert("Masukkan Panjang Sisi");
     }
-
-  }
-  else {
+  } else {
     alert("Object Selected is not a rectangle");
   }
-  
 }
 
+function showObjId(id, htmlid) {
+  let idString = "None";
+  if (id > 0) {
+    idString = String(id);
+  }
+  document.getElementById(htmlid).innerHTML = idString;
+}
 
 function getClickedGLObject() {
   const id = appState.selId;
+  showObjId(id, "sel-id");
   return GLobjectList.find((x) => x.id === id);
+}
+
+function recolorSelectedObject(id) {
+  // console.log(appState);
+  // Recolors the selected object
+  if (id >= 0) {
+    var selObj = GLobjectList.find((x) => x.id === id);
+
+    if (selObj) {
+      // Resets the old object color
+      if (id != appState.oldPick.id) {
+        resetSelObjColor();
+      }
+      appState.oldPickColor = selObj.color;
+      appState.oldPick = selObj.id;
+      // selObj.SetColorByArray([0.9, 0.1, 0.1, 1.0]);
+    }
+  } else {
+    if (appState.oldPick != -1) {
+      resetSelObjColor();
+    }
+    appState.oldPick = -1;
+  }
+
+  function resetSelObjColor() {
+    if (appState.oldPick != -1) {
+      var oldSelObj = GLobjectList.find((x) => x.id === appState.oldPick);
+      oldSelObj.SetColorByArray(appState.oldPickColor);
+    }
+  }
+}
+
+const baseDraggerId = 1000000;
+
+let shaderProgramGlobal;
+
+const draggerRenderer = new Renderer();
+// draggers = draggerRenderer.objectList;
+// draggerRenderer.objectList = draggers;
+function getClickedDragger() {
+  const id = appState.selId;
+  return draggerRenderer.objectList.find((x) => x.id === id);
+}
+
+function createDraggers(obj) {
+  // draggers.clear();
+  draggerRenderer.objectList = [];
+
+  if (obj) {
+    for (let index = 0; index < obj.va.length / 2; index++) {
+      // const element = array[index];
+      console.log("REEEE");
+      var neoDragger = new GLObjectDragger(
+        baseDraggerId + index,
+        shaderProgramGlobal,
+        gl
+      );
+      neoDragger.setVertexId(index);
+      neoDragger.setPosition(obj.va[index * 2], obj.va[index * 2 + 1]);
+      neoDragger.centerOriginsSetOffsetAndFixVertices();
+      neoDragger.assignProjectionMatrix();
+
+      draggerRenderer.addObject(neoDragger);
+    }
+  }
+
+  console.log(draggerRenderer.objectList);
+}
+
+function moveObjVertex(draggerObj, obj) {
+  let oldObjVertex = obj.va;
+  oldObjVertex[draggerObj.vertId * 2] = draggerObj.pos[0];
+  oldObjVertex[draggerObj.vertId * 2 + 1] = draggerObj.pos[1];
+  // let deltaX = appState.mousePos.x - appState.dragStart.x;
+  // let deltaY = appState.mousePos.y - appState.dragStart.y;
+  // oldObjVertex[draggerObj.vertId * 2] += deltaX;
+  // oldObjVertex[draggerObj.vertId * 2 + 1] += deltaY;
+
+  obj.setVertexArray(oldObjVertex);
 }
 
 async function main() {
@@ -127,6 +215,14 @@ async function main() {
         y: event.clientY - bound.top,
       };
       appState.mousePos = res;
+
+      // dragger thingies
+      if (appState.draggedHandle && appState.selectedObject) {
+        appState.draggedHandle.setPosition(res.x, gl.canvas.height - res.y);
+        appState.draggedHandle.assignProjectionMatrix();
+        console.log("reee");
+        moveObjVertex(appState.draggedHandle, appState.selectedObject);
+      }
     },
     false
   );
@@ -143,15 +239,73 @@ async function main() {
   canvas.addEventListener(
     "click",
     function () {
-      let clickedGLObject = getClickedGLObject();
-      if (clickedGLObject) {
-        appState.selectedObject = clickedGLObject;
-        console.log(appState.selectedObject);
-        appState.selectedObject.setColor(0, 0, 0, 1);
-      } else {
-        appState.selectedObject = null;
-        console.log(appState.selectedObject);
+      if (appState.selId < baseDraggerId) {
+        let clickedGLObject = getClickedGLObject();
+        recolorSelectedObject(appState.selId);
+        if (clickedGLObject && clickedGLObject.id < baseDraggerId) {
+          // console.log(appState.selectedObject);
+          appState.selectedObject = clickedGLObject;
+          createDraggers(clickedGLObject);
+          // appState.selectedObject.setColor(0, 0, 0, 1);
+        } else {
+          appState.selectedObject = null;
+          createDraggers(clickedGLObject);
+          // console.log(appState.selectedObject);
+        }
       }
+    },
+    false
+  );
+
+  canvas.addEventListener(
+    "mousedown",
+    function (event) {
+      // store a ref. on the dragged elem
+      let clickedGLObject = getClickedDragger();
+      console.log(clickedGLObject);
+      if (clickedGLObject && clickedGLObject.id >= baseDraggerId) {
+        appState.draggedHandle = clickedGLObject;
+      } else {
+        appState.draggedHandle = null;
+        // console.log(appState.selectedObject);
+      }
+      console.log("D");
+      if (appState.draggedHandle) {
+        console.log("rs");
+        // appState.dragStart = (appState.mousePos.x, appState.mousePos.y);
+        console.log("appState.mousePos : ");
+        console.log(appState.mousePos);
+        const res = {
+          x: appState.mousePos.x,
+          y: appState.mousePos.y,
+        };
+        appState.dragStart = res;
+      }
+      // make it half transparent
+      // event.target.style.opacity = 0.5;
+    },
+    false
+  );
+
+  canvas.addEventListener(
+    "mouseup",
+    function (event) {
+      if (appState.draggedHandle) {
+        console.log("re");
+        console.log(appState.draggedHandle);
+        // appState.dragEnd = (appState.mousePos.x, appState.mousePos.y);
+        appState.draggedHandle.setPosition(
+          appState.mousePos.x,
+          gl.canvas.height - appState.mousePos.y
+        );
+        appState.draggedHandle.assignProjectionMatrix();
+        console.log(appState.draggedHandle);
+        console.log("appState.mousePos : ");
+        console.log(appState.mousePos);
+        moveObjVertex(appState.draggedHandle, appState.selectedObject);
+      }
+      appState.draggedHandle = null;
+      console.log("ag");
     },
     false
   );
@@ -166,42 +320,39 @@ async function main() {
     B = document.getElementById("blue").value / 255;
     G = document.getElementById("green").value / 255;
     appState.selectedObject.setColor(R, G, B, 1);
-  } 
-  );
+  });
 
   // Change Rectangle Side Length
   btnUpdateSquareSideLength.addEventListener("click", updateRectSideLength);
 
-
   // Change Line Length
   btnUpdateLineLength.addEventListener("click", updateLineLength);
-
 
   // const triangleData = [0.0, 0.0, 1.0, 0.0, 0.0, 1.0]; // in clip space
   const triangleData = [400, 400.0, 400.0, 200.0, 200.0, 400.0]; // in pixel space
   // const triangleData = [0, 0, 200, 0, 0, 200]; // in pixel space
   // const triangleData = [-100, -100, 0, 100, 100, -100]; // in pixel space
   const triangleDataCentered = [-100, -100, 0, 100, 100, -100]; // in pixel space
+  const draggerDataCentered = [-10, -10, 0, 10, 10, -10]; // in pixel space
   const triangleDataFromZero = [0, 0, 100, 200, 200, 0]; // in pixel space
   // const triangleData = [100, 100, 200, 300, 300, 100]; // in pixel space
 
-  const lineData = [
-  200.0, 200.0,
-  500.0, 500.0]
+  const lineData = [200.0, 200.0, 500.0, 500.0];
   const rectangleData = [
-    100.0, 100.0,
-    500.0, 100.0,
-    100.0, 500.0,
-    100.0, 500.0,
-    500.0, 100.0,
-    500.0, 500.0,
-  ]
-  const polygonData = [
-    100.0, 200.0,
-    200.0, 200.0,
-    100.0, 100.0,
-    500.0, 100.0,
-  ]
+    100.0,
+    100.0,
+    500.0,
+    100.0,
+    100.0,
+    500.0,
+    100.0,
+    500.0,
+    500.0,
+    100.0,
+    500.0,
+    500.0,
+  ];
+  const polygonData = [100.0, 200.0, 200.0, 200.0, 100.0, 100.0, 500.0, 100.0];
 
   const selectedColor = [0.9, 0.1, 0.1, 1.0];
 
@@ -210,6 +361,8 @@ async function main() {
     "draw-vert.glsl",
     "draw-frag.glsl"
   );
+
+  shaderProgramGlobal = shaderProgram;
 
   // Converts gl program from using clip space (so... 1.0 is the rightmost placement of the canvas) into the pixel space
   gl.useProgram(shaderProgram);
@@ -295,16 +448,16 @@ async function main() {
   // glObject.bind();
   // glObject.draw();
 
-  const glObject = new GLObjectLine(1, shaderProgram, gl);
-  glObject.setVertexArray([100, 100, 200, 200]);
-  glObject.SetColorByArray([1, 0, 0, 1.0]);
+  const glObject = new GLObjectRectangle(1, shaderProgram, gl);
+  // glObject.setVertexArray([100, 100, 200, 200]);
+  glObject.setVertexArray(rectangleData);
+  glObject.SetColorByArray([1, 0, 0, 0.2]);
   glObject.setPosition(0, 0);
   glObject.setRotation(0);
-  glObject.setScale(1, 1);
+  glObject.setScale(0.1, 0.1);
   glObject.centerOriginsSetOffsetAndFixVertices();
   glObject.assignProjectionMatrix();
 
-  /*
   const glObject2 = new GLObject(4, shaderProgram, gl);
   glObject2.setVertexArray(triangleData);
   // glObject2.setVertexArray(triangleData);
@@ -316,7 +469,7 @@ async function main() {
   glObject2.assignProjectionMatrix();
   // glObject2.bind();
   // glObject2.draw();
-
+  /*
   const glObject3 = new GLObject(8, shaderProgram, gl);
   glObject3.setVertexArray(triangleDataFromZero);
   // glObject3.setVertexArray(triangleData);
@@ -338,7 +491,7 @@ async function main() {
   GLobjectList = renderer.objectList;
   console.log("GLobjectList : " + GLobjectList);
   renderer.addObject(glObject);
-  //renderer.addObject(glObject2);
+  renderer.addObject(glObject2);
   //renderer.addObject(glObject3);
   console.log("GLobjectList : " + GLobjectList);
   renderer.render();
@@ -361,7 +514,7 @@ async function main() {
     //   timer++;
     // }
 
-    //rotateAllObjects();
+    // rotateAllObjects();
 
     // actual render function here
     // renderer.objectList[0].va[0] += 1;
@@ -378,6 +531,7 @@ async function main() {
     const resolutionPos = gl.getUniformLocation(selectProgram, "u_resolution");
     gl.uniform2f(resolutionPos, gl.canvas.width, gl.canvas.height);
     renderer.renderTex(selectProgram);
+    draggerRenderer.renderTex(selectProgram);
     // getting the pixel value
     const pixelX = (appState.mousePos.x * gl.canvas.width) / canvas.clientWidth;
     const pixelY =
@@ -388,6 +542,7 @@ async function main() {
     gl.readPixels(pixelX, pixelY, 1, 1, gl.RGBA, gl.UNSIGNED_BYTE, data);
     const id = data[0] + (data[1] << 8) + (data[2] << 16) + (data[3] << 24);
     appState.selId = id;
+    showObjId(id, "hov-id");
     // console.log("Id : " + id);
     // recolorSelectedObject(id);
 
@@ -395,6 +550,7 @@ async function main() {
     gl.bindFramebuffer(gl.FRAMEBUFFER, null);
     // draw the actual objects
     gl.useProgram(shaderProgram);
+    draggerRenderer.render();
     renderer.render();
     requestAnimationFrame(render);
   }
@@ -405,37 +561,6 @@ async function main() {
       element.setRotation(element.rot + 1);
       element.assignProjectionMatrix();
     });
-  }
-
-  function recolorSelectedObject(id) {
-    // Recolors the selected object
-    if (id >= 0) {
-      var selObj = renderer.objectList.find((x) => x.id === id);
-
-      if (selObj) {
-        // Resets the old object color
-        if (id != appState.oldPick.id) {
-          resetSelObjColor();
-        }
-        appState.oldPickColor = selObj.color;
-        appState.oldPick = selObj.id;
-        selObj.SetColorByArray(selectedColor);
-      }
-    } else {
-      if (appState.oldPick != -1) {
-        resetSelObjColor();
-      }
-      appState.oldPick = -1;
-    }
-
-    function resetSelObjColor() {
-      if (appState.oldPick != -1) {
-        var oldSelObj = renderer.objectList.find(
-          (x) => x.id === appState.oldPick
-        );
-        oldSelObj.SetColorByArray(appState.oldPickColor);
-      }
-    }
   }
 
   requestAnimationFrame(render);
